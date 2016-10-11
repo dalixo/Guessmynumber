7b8ae6740f84e1510bd8e2255dd7931253fd1dcc
Gjon Chikonda <revvg@users.noreply.github.com>
2016-10-11 09:26:30 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GuessMyNumberWinForm
+{
+    partial class FormGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(26, 128);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(55, 22);
+            this.txtGuess.TabIndex = 1;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(26, 19);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(323, 66);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(23, 105);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(58, 17);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "Number";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(87, 121);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 37);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(168, 121);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(98, 37);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play Again";
+            this.btnPlay.UseVisualStyleBackColor = true;
+	    this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(274, 121);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 37);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+	    this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // FormGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 191);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "FormGuess";
+            this.Text = "Guess My Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessMyNumberWinForm
+{
+    public partial class FormGuess : Form
+    {
+        public int guess = 0;
+        static Random r = new Random();
+        public int number = r.Next(5);
+        public int tries = 3;
+
+        public FormGuess()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            this.MaximizeBox = false;
+            btnPlay.Enabled = false;
+            lblMessage.Text = "Guess my number. Hint: its between 0 to 5.";
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+	    
+
+	this.Controls.Clear();
+	this.InitializeComponent();
+
+
+        }
+	
+	
+
+	private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+	private void btnClose_Click(object sender, EventArgs e)
+        {
+	this.Close();
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            
+	 
+	if (int.TryParse(txtGuess.Text,out guess))
+	{
+	   if (tries > 0)
+            {
+                if (guess != number)
+                {
+		    tries--;
+                    lblMessage.Text = ("You have guessed wrong, guess again. You have" + " " + (tries) + " " + "guesses left");
+                    number = int.Parse(txtGuess.Text);
+                    
+                }
+		else
+           	{
+		    guess = int.Parse(txtGuess.Text);
+                    btnCheck.Enabled = false;
+                    btnPlay.Enabled = true;
+                    lblMessage.Text = ("You have guessed right, my number is" + " " + (Convert.ToString(number)) + ". Play again for $10?");
+            	}
+            }
+	    
+            else
+	    {
+		btnCheck.Enabled = false;
+                btnPlay.Enabled = true;
+		lblMessage.Text = ("You are out of guesses.");
+                 
+            }
+           }
+
+          else
+	  {
+		lblMessage.Text = ("Please enter a number...Re-enter your guess.");
+	  }
+        }
+    }
+}
+        
+    
